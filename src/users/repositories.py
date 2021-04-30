77886af876d798236f6338523d4cfc952b6a10dc
rhasan33 <rakib.hasan@evaly.com.bd>
2021-04-30 15:39:14 +0600

    update token expiration

--- src/users/repositories.py
+++ src/users/repositories.py
@@ -1,7 +1,5 @@
 from typing import Dict, Union
 
-from peewee import DoesNotExist
-
 from users.models import User
 
 
@@ -9,12 +7,13 @@ def create_user(data: Dict) -> Union[User, bool]:
     try:
         User.get(User.username == data['username'])
         return False
-    except DoesNotExist:
+    except User.DoesNotExist:
         user = User()
         user.username = data['username']
         user.password = user.set_password(raw_password=data['password'])
         user.is_active = True
         user.tags = data.get('tag', list())
         user.name = data['name']
-        user.name = data['email']
+        user.email = data['email']
+        user.save()
         return user
